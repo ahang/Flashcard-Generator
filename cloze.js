5ae478e76b6d936d990a90bad620a7072b41be41
ahang1 <hang.andy.t@gmail.com>
2017-04-11 22:24:18 -0500

    updating a lot of stuff

--- cloze.js
+++ cloze.js
@@ -4,25 +4,22 @@ var ClozeCard = function(text, cloze) {
     this.text = text;
     this.cloze = cloze;
 
-    this.clozeDel = function() {
-        //returns answer
-        //returns "George Washington"
-        console.log("ClozingDel");
-        console.log(this.cloze);
-    }
+    this.partialText = this.text.replace(this.cloze, "...");
+    // {
+    //     //returns question/sentence
+    //     //returns ... was the first president of the United States.
+    //     // console.log("partial");
+    //     console.log(this.text.replace(this.cloze, "..."));
 
-    this.partialText = function() {
-        //returns question/sentence
-        //returns ... was the first president of the United States.
-        console.log("partial");
-    }
+    // }
 
-    this.fullText = function() {
-        //returns full answer imbedded
-        //"George Washington was the first president of the United States."
-        console.log("full text");
-        console.log(this.text);
-    }
+    this.fullText = this.text ;
+    // {
+    //     //returns full answer imbedded
+    //     //"George Washington was the first president of the United States."
+    //     console.log("full text");
+    //     this.text
+    // }
 
 }
 
--- main.js
+++ main.js
@@ -2,11 +2,20 @@ var BasicCard = require("./basic.js");
 var ClozeCard = require("./cloze.js");
 
 var superman = new ClozeCard("Superman was a kryptonian alien", "kryptonian");
-var batman = new BasicCard("Batman is an orphan", "Orphan");
+var spiderman = new ClozeCard("Peter Parker is spiderman", "spiderman");
+var batman = new BasicCard("Batman is ...", "Bruce Wayne");
 
 console.log(superman);
 console.log(batman);
 
+console.log("---------------Superman--------");
 console.log(superman);
-superman.clozeDel();
-superman.fullText();
+console.log(superman.cloze);
+console.log(superman.fullText);
+console.log(superman.partialText);
+console.log("---------------Superman--------");
+
+
+console.log(spiderman.partialText);
+console.log(spiderman.cloze);
+console.log(spiderman.fullText);
