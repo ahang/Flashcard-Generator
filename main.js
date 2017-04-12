a51199f064aff6f75c80d5567dfc48d0f6929940
Andy Hang <hang.andy.t@gmail.com>
2017-04-12 14:54:19 -0500

    testing... inquirer

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 var BasicCard = require("./basic.js");
 var ClozeCard = require("./cloze.js");
+var inquirer = require("inquirer");
+var fs = require("fs");
 
-var superman = ClozeCard("Superman was a kryptonian alien", "kryptonian");
-var spiderman = ClozeCard("Peter Parker is spiderman", "spiderman");
-var batman = new BasicCard("Who is batman?", "Bruce Wayne");
+var startGame = function() {
+    inquirer.prompt([
+        {
+            name: "card",
+            type: "list",
+            message: "What type of Card game would you like to try?",
+            choices: ["ClozeCard", "BasicCard"]
+        }
+    ]).then(function(answers) {
+        console.log(answers);
+    });
+}; 
 
-console.log(batman.front);
-console.log(batman.back);
-
-console.log(superman);
-console.log(batman);
-
-console.log("---------------Superman--------");
-console.log(superman);
-console.log(superman.cloze);
-console.log(superman.fullText);
-console.log(superman.partialText);
-console.log("---------------Superman--------");
+startGame();
 
 
-console.log(spiderman.partialText);
-console.log(spiderman.cloze);
-console.log(spiderman.fullText);
+
+
+
+//Testing out .cloze/.fullText/.partialText
+// var superman = ClozeCard("Superman was a kryptonian alien", "kryptonian");
+// var spiderman = ClozeCard("Peter Parker is spiderman", "spiderman");
+// var batman = new BasicCard("Who is batman?", "Bruce Wayne");
+
+// console.log(batman.front);
+// console.log(batman.back);
+
+// console.log(superman);
+// console.log(batman);
+
+// console.log("---------------Superman--------");
+// console.log(superman);
+// console.log(superman.cloze);
+// console.log(superman.fullText);
+// console.log(superman.partialText);
+// console.log("---------------Superman--------");
+
+
+// console.log(spiderman.partialText);
+// console.log(spiderman.cloze);
+// console.log(spiderman.fullText);
