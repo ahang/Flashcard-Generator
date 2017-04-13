884d4c6b6549d9eebb70ca67d33144230c7ef11a
ahang1 <hang.andy.t@gmail.com>
2017-04-12 23:56:05 -0500

    clozeGame is sort of working

---
+++ clozejson.js
@@ -0,0 +1,4 @@
+exports.qCloze = {
+	"Batman is Bruce Wayne": "Batman",
+	"Damian Wayne is the child of Bruce Wayne and Talia al Ghul. He also takes on the identity of Robin.": "Talia al Ghul"
+};
--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var BasicCard = require("./basic.js");
 var ClozeCard = require("./cloze.js");
+//var clozeCards = require("./clozejson.js");
+
+//console.log(cCards);
+//console.log(Object.keys(clozeCards));
+
 var inquirer = require("inquirer");
 var fs = require("fs");
 
+
+var qCloze = {
+    "Batman is Bruce Wayne": "Batman",
+    "Damian Wayne is the child of Bruce Wayne and Talia al Ghul. He also takes on the identity of Robin.": "Talia al Ghul"
+};
+
+console.log(qCloze);
 var startGame = function() {
     inquirer.prompt([
         {
@@ -13,10 +25,51 @@ var startGame = function() {
         }
     ]).then(function(answers) {
         console.log(answers);
+
+        if (answers.card === "BasicCard") {
+            console.log("This is starting a new inquirer prompt for BasicCard");
+        } else {
+            console.log("This is going to start a new inquirer prompt for ClozeCard");
+            clozeGame();
+        }
     });
 }; 
 
 startGame();
+
+var basicGame = function() {
+
+}
+
+var count = 0;
+var clozeGame = function() {
+    console.log("The count is " + count);
+    console.log(qCloze);
+    var first = Object.keys(qCloze)[count];
+    var second = Object.values(qCloze)[count];
+    var construct = ClozeCard(first, second);
+    //console.log(construct.partialText);
+   // console.log(first);
+
+    // console.log(newQ);
+    inquirer.prompt([
+    {
+        name: "question",
+        message: construct.partialText
+    }
+    ]).then(function(response) {
+        console.log(response);
+        if (response.question === second){
+            console.log("Yes!!");
+        } else {
+            console.log("NO!!");
+        }
+        count++;
+        clozeGame();
+    });
+}
+
+//obj.keys
 
 
 
